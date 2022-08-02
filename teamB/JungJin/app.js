930c7f29fd575dc0da615fff24763c67f8fa3f09
JJ <jjmoon4682@gmial.com>
2022-08-02 12:43:22 +0900

    [ADD] 유저 회원가입  엔드포인트

--- teamB/JungJin/app.js
+++ teamB/JungJin/app.js
@@ -18,9 +18,21 @@ app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(express.json());
 
-
 app.get('/ping', function(req, res){
     res.json({message: 'pong'})
+})
+
+app.get('/users', function(req, res){
+    const query = myDataSource.query(`SELECT * FROM users`, (err, rows) => {
+        res.status(200).json({users : rows});
+    })
+})
+
+app.post('/users', function(req, res){
+    const {name, email, profile_image, password} = req.body
+    const sql = {name:name, email:email, profile_image:profile_image, password:password}
+    const query = myDataSource.query(`INSERT INTO users set ?`, sql)
+    res.status(201).json({message: "userCreated"});
 })
 
 app.listen(3000, function () {
