6903630e97447cb5f0a92726f60ec3dc75c2942a
길길성민 <ksm65501@gmail.com>
2022-08-02 18:38:56 +0900

    [ADD] 유저의 게시물 조회 엔드포인트 구현

--- teamB/seongmin/westagram/app.js
+++ teamB/seongmin/westagram/app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(json());
 app.use(cors());
 app.use(morgan('dev'));
 
@@ -26,10 +26,12 @@ myDataSource.initialize()
         console.log('Data Source has been initialized!')
     })
 
+//server health check
 app.get('/ping', (req, res) => {
     res.status(200).json({ mesaage: 'pong' })
 });
 
+//모든 사용자 조회
 app.get('/users', async (req, res) => {
     await myDataSource.query(`
     SELECT u.id, u.name, u.email, u.profile_image, u.created_at FROM users u 
@@ -38,6 +40,7 @@ app.get('/users', async (req, res) => {
     })
 });
 
+//모든 게시물 조회
 app.get('/posts', async (req, res) => {
     await myDataSource.query(`SELECT
         u.id userId,
@@ -51,6 +54,39 @@ app.get('/posts', async (req, res) => {
         (err, rows) => res.status(200).json({ data: rows }));
 });
 
+//유저 게시물 조회
+app.get('/post/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const posts =[];
+    //유저 정보 조회
+    await myDataSource.query(`SELECT
+        u.id userId,
+        u.profile_image userProfileImage
+        FROM users u
+        WHERE u.id = ${userId}`, (err, data) => {
+            const userData = data;
+            //해당 유저가 작성한 게시물 조회
+            myDataSource.query(`SELECT p.id postingId, p.title postingTitle, p.content postingContent 
+            FROM posts p INNER JOIN users ON p.user_id = users.id WHERE users.id = ${userId};`, (err, datas) => {
+                for (const data of datas) {
+                    posts.push({
+                        postingId : data['postingId'],
+                        postingTitle : data['postingTitle'],
+                        postingContent : data['postingContent'],
+                    })
+                }
+                const user = {
+                    userId : userData[0]['userId'],
+                    userProfileImage : userData[0]['userProfileImage'],
+                    post : posts
+                }
+                res.status(200).json({data: user})
+            })
+        });
+    
+})
+
+//회원가입
 app.post('/signUp', async (req, res) => {
     const { name, email, profile_image, password } = req.body;
     await myDataSource.query(
@@ -62,10 +98,10 @@ app.post('/signUp', async (req, res) => {
         ) VALUES (?, ?, ?, ?);`,
         [name, email, profile_image, password]
     );
-
     res.status(201).json({ mesaage: "userCreated" });
 });
 
+//게시물 작성
 app.post('/addPost', async (req, res) => {
     const { title, content, user_id } = req.body;
     await myDataSource.query(
