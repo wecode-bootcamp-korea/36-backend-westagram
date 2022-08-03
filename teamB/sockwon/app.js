6fe384794bd7163b6cb84e3b352b855ee1fa2778
lee sock won <lsw4uto@gmail.com>
2022-08-03 17:31:42 +0900

    [이석원-teamB] Assignment 1:EXPRESS API 초기환경셋팅

---
+++ teamB/sockwon/app.js
@@ -0,0 +1,54 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config() //환경변수의 조정. 아래에 있었다면 에러가 났을 것이다. 위치에 신경써야.
+
+const {DataSource} = require('typeorm');
+
+
+
+const myDataSource = new DataSource({
+    type: 'mysql',
+    host: process.env.TYPEORM_HOST ,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+})
+
+const PORT = process.env.PORT;
+myDataSource.initialize()
+.then(()=>{console.log("data source has been initialized!")})
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("combined"));
+
+app.get("/ping", async (req, res)=> {
+    res.status(201).json({MESSAGE : "pong"});
+})
+
+app.post("/usersignup", async (req, res)=>{
+    const {name, email, profile_image,password}= req.body;
+    await myDataSource.query(
+        `
+        INSERT INTO users 
+        (name,email, profile_image,password) 
+        VALUES (?,?,?,?);
+        `,
+        [name, email, profile_image,password]
+    );
+    res.status(201).json({MEASSAGE : 'created_success!'});
+})
+
+
+const server = http.createServer(app);
+
+const start = async ()=>{
+    server.listen(PORT, ()=>console.log(`server is listening on port ${PORT}`));
+}
+
+start();
