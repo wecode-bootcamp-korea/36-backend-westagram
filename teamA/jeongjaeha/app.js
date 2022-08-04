49350d320e0ad7607522c98c9ba5d6312e389f90
JeongJaeHa <jaeha2324@gmail.com>
2022-08-04 13:04:15 +0900

    Assignment :6 중간저장

--- teamA/jeongjaeha/app.js
+++ teamA/jeongjaeha/app.js
@@ -115,9 +115,47 @@ app.get('/posts/:id', async (req, res) => {
           posting : rows[i].posting
         }) 
       }
-
       res.status(200).json( data );
     })
+});
+
+//Assignment :6 유저의 게시글 수정하기
+
+// app.patch('/patch', async(req, res) => {
+//   const { user_no, post_no, update_post} = req.body;
+//     await myDataSource.query(
+//       `UPDATE posts 
+//       SET post = ? 
+//       WHERE posts.no = ${post_no} AND posts.users_no = ${user_no}
+//       `,      
+//       [user_no, post_no, update_post]
+//       ,(err, rows) => {
+//         newPost = [];
+
+//         post = {
+//           userId : rows[0].user_id,
+//         }
+
+//         post.push(newPost);
+//       }
+ 
+//     );
+//     res.status(201).json({ newPost });
+// });
+
+app.patch('/patch', async(req, res) => {
+  const { user_no, post_no, update_post} = req.body;
+    await myDataSource.query(
+      `UPDATE posts 
+      SET POSTS = (SELECT POSTS.POST
+        FROM (SELECT POSTS FROM posts ))
+      `
+      ,(err, rows) => {
+
+        res.status(201).json({ rows });
+      }
+    );
+
 });
 
 //create  book
@@ -134,7 +172,7 @@ app.post("/books", async (req, res, next) => {
     [ title, description, coverImage ]
   );
 
-      res.status(201).json({ message: "successfully created"});
+      res.status(200).json({ message: "successfully created"});
 });
 
 //create  authors
