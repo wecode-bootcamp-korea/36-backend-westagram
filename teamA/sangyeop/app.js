3318bcbfe035aea53448bad591e7abb07d771409
Sangyeob Lee <ydeja6@gmail.com>
2022-08-04 10:39:52 +0900

    [ADD]express초기세팅

---
+++ teamA/sangyeop/app.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+
+const dotenv = require("dotenv");
+dotenv.config()
+
+const { DataSource } = require('typeorm');
+
+const myDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE
+})
+
+myDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized");
+  })
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+//서버 작동 확인
+app.get("/ping", (req,res) => {
+  res.json({ message: "pong"});
+});
+
+
+const server = http.createServer(app);
+const PORT = process.env.PORT;
+
+const start = async () => {
+  server.listen(PORT, () => console.log(`Server is listening at ${PORT}`));
+}
+
+start();
