4a86f9c90e5c99d10c62d9540e1ded3383d255ef
MH <myungh5@MHui-MacBookAir.local>
2022-08-05 16:04:14 +0900

    wip

--- teamB/parkmyungho/app.js
+++ teamB/parkmyungho/app.js
@@ -6,168 +6,28 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 dotenv.config(); 
 
-<<<<<<< HEAD
-const {DataSource} = require('typeorm');
-const { ppid } = require("process");
+const routes = require("./routes"); // 폴더 안에 index가 디폴트 값
 
-const appDataSource = new DataSource({
-=======
-const {DataSource} = require('typeorm')
+const app = express();
 
-const myDataSource = new DataSource({
->>>>>>> d789608b13c4c448c4fc34c578078de59fc784fe
-    type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE
-})
-
-<<<<<<< HEAD
-appDataSource.initialize()
-=======
-myDataSource.initialize()
->>>>>>> d789608b13c4c448c4fc34c578078de59fc784fe
-    .then(()=>{
-        console.log("Data Source has been initialized!")
-    });
-
-const app = express()
-
-app.use(express.json());
 app.use(cors());
-app.use(morgan('dev')); 
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(routes);
 
 app.get("/ping", (req, res)=>{
-    res.json({message: "pong"})
-})
+    res.json({message: "pong"});
+});
 
-<<<<<<< HEAD
-//Assingment2 - 회원가입 엔드포인트
-app.post('/user/signup', async(req,res)=>{
-    const {name, email, profileImage, password} = req.body
-
-    await appDataSource.query(
-        `INSERT INTO users(
-            name,
-            email,
-            profile_image,
-            password
-        ) VALUES (?,?,?,?);
-        `,
-        [name, email, profileImage, password]
-    )
-    res.status(201).json({message:"userCreated"})
-})
-//Assingment3 - 게시글 등록 엔드포인트
-app.post('/post/new', async(req,res)=>{
-    const {title, content, userId} = req.body
-
-    await appDataSource.query(
-        `INSERT INTO posts(
-            title,
-            content,
-            user_id
-        ) VALUES (?,?,?);
-        `,
-        [title, content, userId]
-    )
-    res.status(201).json({message:"postCreated"})
-})
-
-//Assignment4 - 전체 게시글 조회 엔드포인트
-app.get('/users-posts', async (req, res)=>{
-    await appDataSource.manager.query(
-        `SELECT
-                users.id UserId,
-                users.profile_image userProfileImage,
-                posts.id postingId,
-                posts.title postingImageUrl,
-                posts.content posingContent
-            FROM users
-            Inner Join posts ON users.id=posts.user_id`
-        ,(err, rows) => {
-                res.status(200).json(rows);
-        })
-})
-
-// assignment5 - 유저 게시글 조회 엔드포인트
-app.get('/posts/:userId', async (req, res) => {
-    const userId = req.params.userId;
-    const posts =[];
-
-    await appDataSource.query(
-        `SELECT
-            u.id userId,
-            u.name userName,
-            u.profile_image userProfileImage
-        FROM users u
-        WHERE u.id = ${userId}`
-        , (err, data) => {
-            const userData = data;
-            appDataSource.query(
-                `SELECT 
-                    p.id postingId,
-                    p.title postingTitle,
-                    p.content postingContent 
-                FROM posts p 
-                INNER JOIN users 
-                ON p.user_id = users.id 
-                WHERE users.id = ${userId}`
-                , (err, datas) => {
-                for (const data of datas) {
-                    posts.push({
-                        postingId : data['postingId'],
-                        postingTitle : data['postingTitle'],
-                        postingContent : data['postingContent'],
-                    })
-                }
-                const user = {
-                    userId : userData[0]['userId'],
-                    userName : userData[0]['userName'],
-                    userProfileImage : userData[0]['userProfileImage'],
-                    post : posts
-                }
-                res.status(200).json({data: user})
-            })
-        });
-})
-
-//Assignment6 - 게시물 수정 엔드포인트
-app.put('/postFix/:userId-:postId', async(req,res) =>{
-    const {userId, postId} = req.params
-    const {content} =req.body
-    await appDataSource.query(
-        `UPDATE posts 
-            SET content = ?
-            WHERE posts.id=${postId};`
-            , [content]
-    );
-
-    await appDataSource.query(
-        `SELECT 
-            u.id userId,
-            u.name userName,
-            p.id postingId,
-            p.title postingTitle,
-            p.content postingContent
-        FROM users u
-        INNER JOIN posts p ON u.id = ${userId}
-        WHERE p.id=${postId};`
-        ,(err,rows) => {
-        res.status(206).json({data : rows})
-        }
-    );
-})
-
-=======
->>>>>>> d789608b13c4c448c4fc34c578078de59fc784fe
-const server = http.createServer(app)
+const server = http.createServer(app);
 const PORT = process.env.PORT;
 
-const start = async()=>{
-    server.listen(PORT, ()=> console.log(`server is listening on ${PORT}`))
-}
+const start = async() => {
+    try{
+        server.listen(PORT, ()=> console.log(`Server is listening on ${PORT}`));
+    } catch(err) {
+        console.error(err);
+    }
+};
 
-start()
+start();
---
+++ teamB/parkmyungho/routes/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const router = express.Router();
+
+const userRouter = require("./userRouter");
+//users 일 때 userRouter 파일의 router 모듈을 불러온다.
+router.use("/users", userRouter.router);
+
+
+module.exports = {
+    router
+}
