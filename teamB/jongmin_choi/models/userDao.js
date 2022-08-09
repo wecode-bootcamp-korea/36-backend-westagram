67d14a4cd2261cb2d899bc59068a0cc89bb5db47
joeychoi7624@gmail.com <43774005+Joeychoi7624@users.noreply.github.com>
2022-08-09 12:34:03 +0900

    [Fiexd] repeating initialize data

--- teamB/jongmin_choi/models/userDao.js
+++ teamB/jongmin_choi/models/userDao.js
@@ -1,22 +1,6 @@
-const { DataSource } = require('typeorm');
+const { myDataSource, intialization } = require('./dataSource');
 
-const myDataSource = new DataSource({
-    type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE
-});
-
-myDataSource.initialize()
-.then(() => {
-    console.log("Data source has been initialized!");
-})
-.catch((err) => {
-    console.error("Error during Data Source initialization", err);
-    myDataSource.destroy();
-});
+intialization;
 
 const createUser = async ( name, email, profileImage, password) => {
     try {
