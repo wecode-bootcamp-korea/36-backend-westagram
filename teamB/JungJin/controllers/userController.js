d562d49423ef483332145a4f345b0c3bd265c01e
JJ <jjmoon4682@gmial.com>
2022-08-12 22:15:25 +0900

    [문정진 - teamB] Assignment: Restful API 적용 수정완료 (예비)

--- teamB/JungJin/controllers/userController.js
+++ teamB/JungJin/controllers/userController.js
@@ -19,9 +19,15 @@ const signup = async (req, res) => {
 };
 
 const lookup = async (req, res) => {
-    const query = req.query
-    const users = await userService.lookup(query);
-    res.status(200).json({users : users})
+    try {
+        const query = req.query
+        const users = await userService.lookup(query);
+        res.status(200).json({users : users})
+    }
+    catch (err) {
+        console.log(err)
+        res.status(err.statusCode || 500).json({message : err.message})
+    }
 };
 
 const login = async (req, res) => {
