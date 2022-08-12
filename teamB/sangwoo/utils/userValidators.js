91025c27d97eb0bc6f39f75518926dfe907b25ee
ll04 <sangw1104@gmail.com>
2022-08-12 17:10:05 +0900

    ADD RegExp $

--- teamB/sangwoo/utils/userValidators.js
+++ teamB/sangwoo/utils/userValidators.js
@@ -5,7 +5,7 @@ const validateEmailPw = (email, password) => {
     );
 
     const pwValidation = new RegExp(
-        /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})/
+        /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20}$)/
     );
 
     if (!emValidation.test(email) || !pwValidation.test(password)) {
