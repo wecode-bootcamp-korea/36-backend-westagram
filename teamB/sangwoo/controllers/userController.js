084ce880d65cac5e045db1ca7b4418eeb8343fe2
ll04 <sangw1104@gmail.com>
2022-08-14 17:45:07 +0900

    Modify the authorization and authorization

--- teamB/sangwoo/controllers/userController.js
+++ teamB/sangwoo/controllers/userController.js
@@ -24,7 +24,7 @@ const signIn = async (req, res) => {
     const { email, password } = req.body;
 
     const accessToken = await userService.signIn(email, password);
-
+    
     res.status(200).json({ accessToken: accessToken });
   } catch (err) {
     res
